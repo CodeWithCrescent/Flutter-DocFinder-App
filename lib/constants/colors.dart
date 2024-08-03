69da03438288fac471ca7794a237b049cc3b267e
Crescent Geniius <crescentbeatz31@gmail.com>
2024-08-03 03:32:35 +0300

    App constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,11 @@
+import 'package:hexcolor/hexcolor.dart';
+
+class GlobalColor {
+  static HexColor primary = HexColor('#0165fc');
+  static HexColor white = HexColor('#f8f9fa');
+  static HexColor bg = HexColor('#eaecf0');
+  static HexColor grey = HexColor('#6c757d');
+  static HexColor black = HexColor('#242424');
+  static HexColor dark = HexColor('#484848');
+  static HexColor muted = HexColor('#adb5bd');
+}
