c56865b9c44539a490689237b0738d94f9b8b451
Crescent Geniius <crescentbeatz31@gmail.com>
2024-08-05 20:22:44 +0300

    Created the onboarding pages

--- lib/screens/onboarding_screen.dart
+++ lib/screens/onboarding_screen.dart
@@ -17,6 +17,14 @@ class OnBoardingScreen extends StatelessWidget {
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
           backgroundColor: GlobalColor.primary,
         ),
+        onFinish: () {
+          debugPrint('To Register page!!');
+          Navigator.of(context).pushReplacementNamed('/register');
+        },
+        trailingFunction: () {
+          debugPrint('To Login Page!!');
+          Navigator.of(context).pushNamed('/login');
+        },
         skipTextButton: const Text('Skip'),
         trailing: const Text('Login'),
         centerBackground: true,
@@ -29,35 +37,167 @@ class OnBoardingScreen extends StatelessWidget {
         speed: 1.8,
         pageBodies: [
           Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
             padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: const Column(
+            child: Column(
               children: <Widget>[
-                SizedBox(
+                const SizedBox(
                   height: 480,
                 ),
-                Text('Description Text 1'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Discover',
+                      style: TextStyle(
+                        color: GlobalColor.black,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      'Experienced',
+                      softWrap: true,
+                      style: TextStyle(
+                        color: GlobalColor.primary,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Doctors',
+                  softWrap: true,
+                  style: TextStyle(
+                    color: GlobalColor.primary,
+                    fontSize: 28,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height / 30),
+                Text(
+                  'Book appointments effortlessly and manage your health journey.',
+                  softWrap: true,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: GlobalColor.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
               ],
             ),
           ),
           Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
             padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: const Column(
+            child: Column(
               children: <Widget>[
-                SizedBox(
+                const SizedBox(
                   height: 480,
                 ),
-                Text('Description Text 2'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Learn About',
+                      style: TextStyle(
+                        color: GlobalColor.primary,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      'Your',
+                      softWrap: true,
+                      style: TextStyle(
+                        color: GlobalColor.black,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Doctors',
+                  softWrap: true,
+                  style: TextStyle(
+                    color: GlobalColor.black,
+                    fontSize: 28,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height / 30),
+                Text(
+                  'Book appointments effortlessly and manage your health journey.',
+                  softWrap: true,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: GlobalColor.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
               ],
             ),
           ),
           Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
             padding: const EdgeInsets.symmetric(horizontal: 40),
-            child: const Column(
+            child: Column(
               children: <Widget>[
-                SizedBox(
+                const SizedBox(
                   height: 480,
                 ),
-                Text('Description Text 3'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Effortless',
+                      style: TextStyle(
+                        color: GlobalColor.black,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      'Appointment',
+                      softWrap: true,
+                      style: TextStyle(
+                        color: GlobalColor.primary,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Booking',
+                  softWrap: true,
+                  style: TextStyle(
+                    color: GlobalColor.black,
+                    fontSize: 28,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: MediaQuery.of(context).size.height / 30),
+                Text(
+                  'Book appointments effortlessly and manage your health journey.',
+                  softWrap: true,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: GlobalColor.grey,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
               ],
             ),
           ),
