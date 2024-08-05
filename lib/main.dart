943d1045afaaf3112a1e289ed7f9bb799ba051d4
Crescent Geniius <crescentbeatz31@gmail.com>
2024-08-05 20:20:45 +0300

    Updated the app route names

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:doc_finder/constants/colors.dart';
+import 'package:doc_finder/screens/auth/auth_screen.dart';
 import 'package:doc_finder/screens/get_started.dart';
 import 'package:doc_finder/screens/home_screen.dart';
+import 'package:doc_finder/screens/location_access.dart';
+import 'package:doc_finder/screens/location_manually.dart';
 import 'package:doc_finder/screens/onboarding_screen.dart';
 import 'package:doc_finder/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
@@ -26,6 +29,13 @@ class MyApp extends StatelessWidget {
         '/': (context) => const SplashScreen(),
         '/get-started': (context) => const GetStarted(),
         '/onboarding': (context) => const OnBoardingScreen(),
+        '/login': (context) => AuthScreen(pageNumber: 1),
+        '/register': (context) => AuthScreen(pageNumber: 2),
+        '/forgot-password': (context) => AuthScreen(pageNumber: 3),
+        '/verify-code': (context) => AuthScreen(pageNumber: 4),
+        '/new-password': (context) => AuthScreen(pageNumber: 5),
+        '/location-access': (context) => const LocationAccess(),
+        '/enter-location-manually': (context) => const EnterLocationManually(),
         '/home': (context) => const HomeScreen(),
       },
     );
